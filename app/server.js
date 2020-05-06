08d49edb6aefa03e98b248ce4ec11c8df570959d
Miguel <pimentel611@gmail.com>
2020-05-06 03:12:42 -0400

    Finished Node App

--- app/server.js
+++ app/server.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 var issues = require('../node_modules/issue-manager');
 var path = require('path');
+var jade = require('jade');
+var bodyParser = require('body-parser');
+
+
 
 var issueRouter = require('../routes/issues');
 var usersRouter = require('../routes/users');
-app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.set('views', '../views');
 app.set('view engine', 'jade');
 app.use('/', issueRouter);
 app.use('/users', usersRouter);
--- routes/issues.js
+++ routes/issues.js
@@ -2,22 +2,45 @@ var express = require('express');
 var router = express.Router();
 var issue = require('../node_modules/issue-manager');
 
-router.get('/issues', function (req, res) {
+router.get('/issues', async function (req, res, next) {
+    var issues = await issue.getIssues();
     res.render('issues', {
         title: 'Issues',
-        content: issue.getIssues(res)
+        written: issues
     });
 });
-router.put('/issues/issues.json', function (req, res) {
-    issue.makeIssues(req);
+router.get('/issues/:issue_id.json', async function (req, res, next) {
+    var doc = await issue.getSingleIssue(req.params.issue_id);
+    res.render('issue', {
+        id: doc._id,
+        title: doc.title,
+        status: doc.status,
+        name: doc.assignee,
+        created: doc.created,
+        updated: doc.updated,
+        details: doc.details
+    });
 });
-router.post('/issues/:issue_id.json', function (req, res) {
-    issue.getIssues(res);
+router.get('/issues/:issue_id.json/updt', async function (req, res, next) {
+    var doc = await issue.getSingleIssue(req.params.issue_id);
+    res.render('issueUpdate', {
+        id: req.params.issue_id,
+        title: doc.title,
+        status: doc.status,
+        name: doc.assignee,
+        details: doc.details
+    });
+});
+router.put('/issues/issues.json', async function (req, res, next) {
+    var json_body = req.body;
+    issue.makeIssues(json_body);
+});
+router.post('/issues/:issue_id.json', async function (req, res, next) {
+    var json_body = req.body;
+    await issue.updtIssues(req.params.issue_id, json_body);
+    res.redirect('/issues');
+});
+router.delete('/issues/:issue_id.json/del', async function (req, res, next) {
     issue.delIssues(req.params.issue_id);
-    issue.makeIssues(req);
 });
-router.delete('/issues/:issue_id.json', function (req, res) {
-    issue.delIssues(req.params.issue_id);
-});
-
 module.exports = router;
